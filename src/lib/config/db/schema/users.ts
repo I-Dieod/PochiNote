b9cd8fd196bdb149f2fb87f4dd1bacf64776d91f
IrorI-DIEOD <IrorI.DIEOD@gmail.com>
2025-09-03 22:21:39 +0900

    Update data structure

--- src/lib/config/db/schema/users.ts
+++ src/lib/config/db/schema/users.ts
@@ -1,12 +1,11 @@
 // src/lib/config/db/schema/users.ts
 
 //usersテーブルの定義
-import { pgTable, serial, varchar, timestamp, uuid } from "drizzle-orm/pg-core"
+import { pgTable, serial, varchar, timestamp } from "drizzle-orm/pg-core"
 
 export const users = pgTable("users", {
     tableId: serial("table_id").notNull().primaryKey(),
     userName: varchar("user_name", { length: 100 }).notNull(), // ユーザー名
-    uuid: uuid("uuid").defaultRandom().notNull().unique(), // UUID 型で一意の ID
     email: varchar("email", { length: 255 }).notNull().unique(), // メールアドレス
     password: varchar("password", { length: 255 }).notNull(), // ハッシュ化されたパスワード
     registeredAt: timestamp("registered_at").defaultNow().notNull(), // 登録日時
