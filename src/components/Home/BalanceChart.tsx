d3e3560d59b9a62ffb83c5dc27f48d1a0178764a
IrorI-DIEOD <IrorI.DIEOD@gmail.com>
2025-09-18 10:44:40 +0900

    Update Drop down menu open function

--- src/components/Home/BalanceChart.tsx
+++ src/components/Home/BalanceChart.tsx
@@ -14,55 +14,11 @@ import {
     YAxis,
 } from 'recharts';
 
+import { DropdownMenu } from './SubComponents/DropDownMenu';
 import { dropdownMenuAtom, dropdownOpenAtom, selectedPeriodAtom } from '@/atoms/BalanceChart.atom';
 import { transactionsAtom } from '@/atoms/TransactionTable.atom';
 import { authTokenAtom } from '@/atoms/auth/auth.atom';
 
-const DropdownMenu = () => {
-    const [dropdownOpen, setDropdownOpen] = useAtom(dropdownOpenAtom);
-    const [dropdownMenuItems] = useAtom(dropdownMenuAtom);
-    const [selectedPeriod, setSelectedPeriod] = useAtom(selectedPeriodAtom);
-
-    // TODO: メニュー外クリックで閉じる処理を追加
-    return (
-        <div className="relative inline-block text-left">
-            <button
-                onClick={() => setDropdownOpen((open) => !open)}
-                id="dropdownDefaultButton" data-dropdown-toggle="dropdown"
-                className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                type="button"
-            >
-                {selectedPeriod}
-                <svg className="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
-                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 4 4 4-4" />
-                </svg>
-            </button>
-            {/* Dropdown menu */}
-            {dropdownOpen && (
-                <div id="dropdown" className="absolute mt-2 z-10 bg-white divide-y divide-gray-100 rounded-lg shadow-sm w-44 dark:bg-gray-700" >
-                    <ul className="py-2 text-sm text-gray-700 dark:text-gray-200">
-                        {dropdownMenuItems.map((item) => (
-                            <li key={item.name}>
-                                <a
-                                    className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
-                                    aria-labelledby='dropdownDefaultButton'
-                                    onClick={() => {
-                                        if (item.name === 'Week') setSelectedPeriod('Week');
-                                        else if (item.name === 'Month') setSelectedPeriod('Month');
-                                        else if (item.name === 'Year') setSelectedPeriod('Year');
-                                        else setSelectedPeriod('Max');
-                                    }}
-                                >
-                                    {item.name}
-                                </a>
-                            </ li>
-                        ))}
-                    </ul>
-                </div>
-            )}
-        </div>
-    );
-};
 
 export function BalanceChart() {
     const [authToken] = useAtom(authTokenAtom);
@@ -202,7 +158,6 @@ export function BalanceChart() {
 
         return () => window.removeEventListener('resize', updateDimensions);
     }, []);
-
 
     return (
         <div className="md:w-2/3 sm:w-1/3 rounded-md border-double border-4 border-gray-200">
