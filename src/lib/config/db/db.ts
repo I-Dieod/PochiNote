ea704bdedcc995e1387d76ab5e027f18a615f6c9
IrorI-DIEOD <IrorI.DIEOD@gmail.com>
2025-09-23 23:02:44 +0900

    Update to connect DB function for release

--- src/lib/config/db/db.ts
+++ src/lib/config/db/db.ts
@@ -4,11 +4,7 @@ import { drizzle } from "drizzle-orm/node-postgres";
 import { Pool } from "pg";
 
 const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: Number(process.env.DB_PORT),
+    connectionString: process.env.DATABASE_URL,
 });
 
 export const db = drizzle(pool);
