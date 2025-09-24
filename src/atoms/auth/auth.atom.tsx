b201e49e62012343d25ef2d41a4b6418c7937afb
IrorI-DIEOD <IrorI.DIEOD@gmail.com>
2025-09-24 18:15:15 +0900

    Add auth atoms

---
+++ src/atoms/auth/auth.atom.tsx
@@ -0,0 +1,12 @@
+// src/atoms/auth.atom.tsx
+
+import { atom } from "jotai";
+
+export const UserNameAtom = atom<string>("");
+export const MailAtom = atom<string>("");
+export const PasswordAtom = atom<string>("");
+export const ConfirmPasswordAtom = atom<string>("");
+export const ErrorMessageAtom = atom<string>("");
+
+export const isLogedInAtom = atom<boolean>(false);
+export const authTokenAtom = atom<string | null>(null);
